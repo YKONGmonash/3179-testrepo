98297196a58d4a309b921f6e1073d4967aeaef77
YKONGmonash <90377803+YKONGmonash@users.noreply.github.com>
2021-10-14 15:23:56 +0800

    Update vega_lite_w9hw.js

--- js/vega_lite_w9hw.js
+++ js/vega_lite_w9hw.js
@@ -1,4 +1,4 @@
-var vg_1 = "data/w9hw.vg.json"; 
+var vg_1 = "data/ausWFdata.vg.json"; 
 vegaEmbed("#aus_heat_map", vg_1).then(function (result) { 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
