4a8c9eba5d12909ad31956b64cfe01e74f02ed31
YKONGmonash <90377803+YKONGmonash@users.noreply.github.com>
2021-10-14 13:15:31 +0800

    Add files via upload

--- js/vega_lite_w9hw.js
+++ js/vega_lite_w9hw.js
@@ -9,6 +9,6 @@ vegaEmbed("#mean_min_temp_bar_chart", vg_2).then(function (result) {
 }).catch(console.error);
 
 var vg_3 = "data/mean_max.vg.json"; 
-vegaEmbed("#mean_max_temp_bar_chart", vg_2).then(function (result) { 
+vegaEmbed("#mean_max_temp_bar_chart", vg_3.then(function (result) { 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
